b83bd3ccf4a6fb15351087d458e550395d1fb343
Cyriac Kandoth <ckandoth@gmail.com>
2019-04-09 22:12:06 -0400

    Turn tnr/vaf filters to soft filters for MuTect

--- filter_mutect.py
+++ filter_mutect.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument("-v", "--verbose", action="store_true", dest="verbose", help="make lots of noise")
     parser.add_argument("-ivcf", "--inputVcf", action="store", dest="inputVcf", required=True, type=str, metavar='SomeID.vcf', help="Input vcf muTect file which needs to be filtered")
     parser.add_argument("-itxt", "--inputTxt", action="store", dest="inputTxt", required=True, type=str, metavar='SomeID.txt', help="Input txt muTect file which needs to be filtered")
-    parser.add_argument("-tsn", "--tsampleName", action="store", dest="tName", required=True, type=str, metavar='SomeName', help="Name of the tumor sample")
+    parser.add_argument("-tsn", "--tsampleName", action="store", dest="tsampleName", required=True, type=str, metavar='SomeName', help="Name of the tumor sample")
     parser.add_argument("-rf", "--refFasta", action="store", dest="refFasta", required=True, type=str, metavar='ref.fa', help="Reference genome in fasta format")
     parser.add_argument("-dp", "--totaldepth", action="store", dest="minDP", required=False, type=int, default=5, metavar='5', help="Minimum Tumor total depth")
     parser.add_argument("-ad", "--alleledepth", action="store", dest="minAD", required=False, type=int, default=3, metavar='3', help="Minimum Tumor allele depth")
@@ -60,20 +60,18 @@ def RunStdFilter(args):
     vcf_reader.filters[tnr_tag] = VcfFilter(tnr_tag, 'Non-hotspot with ratio between Tumor-Normal VAFs <' + str(args.minTNR))
 
     allsamples = list(vcf_reader.samples)
-    if(len(allsamples) != 2):
-        if(args.verbose):
+    if len(allsamples) != 2:
+        if args.verbose:
             logger.critical("The VCF does not have two genotype columns. Please input a proper vcf with Tumor/Normal columns")
         sys.exit(1)
 
     # If the caller reported the normal genotype column before the tumor, swap those around
-    if_swap_sample = False
-    if(allsamples[1] == args.tName):
-        if_swap_sample = True
+    swap_sample_cols = False
+    if allsamples[1] == args.tsampleName:
+        swap_sample_cols = True
         vcf_reader.samples[0] = allsamples[1]
         vcf_reader.samples[1] = allsamples[0]
-
-    # Dictionary to store records to keep
-    keepDict = {}
+    nsampleName = vcf_reader.samples[1]
 
     # If provided, load hotspots into a dictionary for quick lookup
     hotspot = {}
@@ -83,73 +81,49 @@ def RunStdFilter(args):
             genomic_locus = str(record.CHROM) + ":" + str(record.POS)
             hotspot[genomic_locus] = True
 
+    # Parse the MuTect text file to figure out which events to keep
+    keepDict = {}
     with open(args.inputTxt, 'rb') as infile:
         reader = csv.DictReader((row for row in infile if not row.startswith('#')), delimiter='\t')
         for row in reader:
-            chr = row['contig']  # Get Chromosome
-            pos = row['position']  # Get Position
-            ref_allele = row['ref_allele']
-            alt_allele = row['alt_allele']
-            trd = int(row['t_ref_count'])
-            tad = int(row['t_alt_count'])
-            tdp = trd + tad
-            tvf = int(tad)/float(tdp) if(tdp != 0) else 0
-            nrd = int(row['n_ref_count'])
-            nad = int(row['n_alt_count'])
-            ndp = nrd + nad
-            nvf = int(nad) / float(ndp) if(ndp != 0) else 0
-            judgement = row['judgement']  # Get REJECT or PASS
-            failure_reason = row['failure_reasons']  # Get Reject Reason
-            nvfRF = int(args.minTNR) * nvf
-
-            # This will help in filtering VCF
-            key_for_tracking = str(chr) + ":" + str(pos) + ":" + str(ref_allele) + ":" + str(alt_allele)
-            locus = str(chr) + ":" + str(pos)
-            if(judgement == "KEEP"):
-                if(key_for_tracking in keepDict):
-                    print("MutectStdFilter:There is a repeat ", key_for_tracking)
-                else:
-                    keepDict[key_for_tracking] = judgement
-            else:
-                rescued_tags = ["alt_allele_in_normal", "nearby_gap_events", "triallelic_site", "possible_contamination", "clustered_read_position"]
-                failure_tags = failure_reason.split(",")
-                tag_count = 0
-                for tag in failure_tags:
-                    if tag in rescued_tags:
-                        tag_count = tag_count + 1
-                    else:
-                        continue
-                if(tag_count != len(failure_tags)):
-                    continue
-                if tvf > nvfRF or locus in hotspot:
-                    if((tdp >= int(args.minDP)) & (tad >= int(args.minAD)) & (tvf >= float(args.minVAF))):
-                        if(key_for_tracking in keepDict):
-                            print("MutectStdFilter:There is a repeat ", key_for_tracking)
-                        else:
-                            keepDict[key_for_tracking] = failure_reason
+            key_for_tracking = str(row['contig']) + ":" + str(row['position']) + ":" + str(row['ref_allele']) + ":" + str(row['alt_allele'])
+            rescued_tags = ["alt_allele_in_normal", "nearby_gap_events", "triallelic_site", "possible_contamination", "clustered_read_position"]
+            if row['judgement'] == "KEEP" or set(row['failure_reasons'].split(",")).issubset(rescued_tags):
+                keepDict[key_for_tracking] = "None" if row['judgement'] == "KEEP" else row['failure_reasons']
 
     vcf_writer = vcf.Writer(open(vcf_out, 'w'), vcf_reader)
     for record in vcf_reader:
-        key_for_tracking = str(record.CHROM) + ":" + str(record.POS) + ":" + str(record.REF) + ":" + str(record.ALT[0])
-        if(key_for_tracking in keepDict):
-            failure_reason = keepDict.get(key_for_tracking)
-            if(failure_reason == "KEEP"):
-                failure_reason = "None"
-            record.add_info('FAILURE_REASON', failure_reason)
-            record.add_info('set', 'MuTect')
-            if (if_swap_sample):
-                nrm = record.samples[0]
-                tum = record.samples[1]
-                record.samples[0] = tum
-                record.samples[1] = nrm
-            if(record.FILTER == "PASS"):
-                vcf_writer.write_record(record)
-            else:
-                record.FILTER = "PASS"
-                vcf_writer.write_record(record)
-
+        tcall = record.genotype(args.tsampleName)
+        tdp = int(tcall['DP']) if(tcall['DP'] is not None) else 0
+        tad = int(tcall['AD'][1]) if(tcall['AD'][1] is not None) else 0
+        tvf = int(tad)/float(tdp) if(tdp != 0) else 0
+        ncall = record.genotype(nsampleName)
+        if ncall:
+            ndp = int(ncall['DP']) if(ncall['DP'] is not None) else 0
+            nad = int(ncall['AD'][1]) if(ncall['AD'][1] is not None) else 0
+            nvf = nad/ndp if(ndp != 0) else 0
+            nvfRF = int(args.minTNR) * nvf
         else:
-            continue
+            logger.critical("filter_mutect: There are no genotype values for Normal. We will exit.")
+            sys.exit(1)
+        locus = str(record.CHROM) + ":" + str(record.POS)
+        record.add_info('set', 'MuTect')
+
+        if swap_sample_cols:
+            nrm = record.samples[0]
+            tum = record.samples[1]
+            record.samples[0] = tum
+            record.samples[1] = nrm
+        key_for_tracking = locus + ":" + str(record.REF) + ":" + str(record.ALT[0])
+        if key_for_tracking in keepDict and tdp >= int(args.minDP) and tad >= int(args.minAD):
+            # Add some FILTER and INFO tags to the remaining events
+            record.FILTER = []
+            if tvf < float(args.minVAF):
+                record.add_filter(vaf_tag)
+            if tvf < nvfRF and locus not in hotspot:
+                record.add_filter(tnr_tag)
+            record.add_info('FAILURE_REASON', keepDict.get(key_for_tracking))
+            vcf_writer.write_record(record)
     vcf_writer.close()
     # Normalize the events in the VCF, produce a bgzipped VCF, then tabix index it
     norm_gz_vcf = cmo.util.normalize_vcf(vcf_out, args.refFasta)
